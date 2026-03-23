e60a8ef3afa766124e5c205a9b572a3e5e687745
Weiyi Zhang <WeiyiZhang3@cmail.carleton.ca>
2026-03-23 12:36:16 -0400

    Thank you message update

--- file.js
+++ file.js
@@ -239,3 +239,14 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+//------- CONTACT THANK YOU MESSAGE ------//
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  alert("Thank you! Your request has been submitted.");
+
+  form.reset();
+});
