cbb1da06b0233f8f98dd487210f2fa1674580332
EricaD07 <ej10.dionne@gmail.com>
2026-03-26 21:42:55 -0400

    contact fixed+sticky nav

--- file.js
+++ file.js
@@ -289,15 +289,15 @@ const message = document.getElementById("thankYouMessage");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  message.textContent = "Thank you for your message! We will get back to you soon.";
+  message.textContent = "Thank you for reserving at MochaMelt! You will recieve an email upon confirmation.";
   message.style.display = "block";
 
   form.reset();
 
-  // hide message after 5 seconds
+  // hide message after 10 seconds
   setTimeout(() => {
     message.style.display = "none";
-  }, 5000);
+  }, 10000);
 });
 
 
